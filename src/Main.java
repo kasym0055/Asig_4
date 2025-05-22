1ea59ac702315c88f0d971dbf8d5412f0818194a
Kasymbek <kasymhr@gmail.com>
2025-05-22 10:02:43 +0500

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Vertex<String> a = new Vertex<>("A");
+        Vertex<String> b = new Vertex<>("B");
+        Vertex<String> c = new Vertex<>("C");
+        Vertex<String> d = new Vertex<>("D");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        WeightedGraph<String> graph = new WeightedGraph<>();
+        graph.addEdge(a, b, 1);
+        graph.addEdge(b, c, 2);
+        graph.addEdge(a, c, 4);
+        graph.addEdge(c, d, 1);
+
+
+        BreadthFirstSearch<String> bfs = new BreadthFirstSearch<>(graph, a);
+        System.out.println("BFS path A to D: " + bfs.getPath(d));
+
+
+        DijkstraSearch<String> dijkstra = new DijkstraSearch<>(graph, a);
+        System.out.println("Dijkstra path A to D: " + dijkstra.getPath(d));
     }
 }
